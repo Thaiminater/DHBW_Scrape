1484333e482a0d2032ddb84c68fe101d948f0406
Thomas Schühly <thomas.schuehly@outlook.com>
2018-03-11 13:13:26 +0100

    updated northdata to generat the urls

--- scraper/northdata.py
+++ scraper/northdata.py
@@ -1,6 +1,7 @@
 import openpyxl as op
 import dryscrape
 from bs4 import BeautifulSoup
+import urllib as ul
 dryscrape.start_xvfb()                                                         # Start dryscrape session
 session = dryscrape.Session()
 
@@ -8,14 +9,22 @@ def northdata(input,col):
 	wb = op.load_workbook(input)
 	print type(wb)
 	sheet = wb['Sheet1']
-	session.visit('https://www.northdata.de/')
-	searchinput = session.at_xpath('//*[@id="search"]/div[1]/form/div/div[1]/input')
-	searchform = session.at_xpath('//*[@id="search"]/div[1]/form')
-	for i in range(2,3):
+
+	# searchinput = session.at_xpath('//*[@id="search"]/div[1]/form/div/div[1]/input')
+	# searchform = session.at_xpath('//*[@id="search"]/div[1]/form')
+	for i in range(3,4):
+		url = "https://www.northdata.de/"
 		firma = sheet.cell(row=i, column=1).value
-		searchinput.set('firma')
-		searchform.submit()
-		#session.render('north.png')
+		firma = ul.quote(firma)
+		searchurl = url + firma
+		print searchurl
+		session.visit(searchurl)
+		session.render('north.png', width = 1900 , height = 1024)
+		body = session.body()
+		soup = BeautifulSoup(body)
+		#print soup.prettify()
+		for data in soup.find_all('div', class_='tab_content'):
+			print data
 		sheet.cell(row=i, column=col).value = 'Test'
 	out = input + '_update.xlsx'
 	wb.save(out)
